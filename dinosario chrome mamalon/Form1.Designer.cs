de7d7f31fe998536b2ca0e4bbc1e2a1e6f660166
IngMauVega <73960971+IngMauVega@users.noreply.github.com>
2020-11-04 13:09:55 -0600

    Add files via upload

---
+++ dinosario chrome mamalon/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace dinosario_chrome_mamalon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Carretera = new System.Windows.Forms.PictureBox();
+            this.Dino = new System.Windows.Forms.PictureBox();
+            this.DetectorChoque = new System.Windows.Forms.PictureBox();
+            this.BtnReset = new System.Windows.Forms.PictureBox();
+            this.lblGameover = new System.Windows.Forms.Label();
+            this.lblpuntuacion = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Carretera)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dino)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DetectorChoque)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BtnReset)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Carretera
+            // 
+            this.Carretera.Image = ((System.Drawing.Image)(resources.GetObject("Carretera.Image")));
+            this.Carretera.Location = new System.Drawing.Point(12, 267);
+            this.Carretera.Name = "Carretera";
+            this.Carretera.Size = new System.Drawing.Size(735, 5);
+            this.Carretera.TabIndex = 0;
+            this.Carretera.TabStop = false;
+            // 
+            // Dino
+            // 
+            this.Dino.BackColor = System.Drawing.Color.Transparent;
+            this.Dino.Image = ((System.Drawing.Image)(resources.GetObject("Dino.Image")));
+            this.Dino.Location = new System.Drawing.Point(12, 217);
+            this.Dino.Name = "Dino";
+            this.Dino.Size = new System.Drawing.Size(100, 50);
+            this.Dino.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Dino.TabIndex = 1;
+            this.Dino.TabStop = false;
+            // 
+            // DetectorChoque
+            // 
+            this.DetectorChoque.Location = new System.Drawing.Point(55, 192);
+            this.DetectorChoque.Name = "DetectorChoque";
+            this.DetectorChoque.Size = new System.Drawing.Size(21, 70);
+            this.DetectorChoque.TabIndex = 2;
+            this.DetectorChoque.TabStop = false;
+            // 
+            // BtnReset
+            // 
+            this.BtnReset.BackColor = System.Drawing.Color.Transparent;
+            this.BtnReset.Image = ((System.Drawing.Image)(resources.GetObject("BtnReset.Image")));
+            this.BtnReset.Location = new System.Drawing.Point(167, 74);
+            this.BtnReset.Name = "BtnReset";
+            this.BtnReset.Size = new System.Drawing.Size(84, 49);
+            this.BtnReset.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BtnReset.TabIndex = 3;
+            this.BtnReset.TabStop = false;
+            this.BtnReset.Visible = false;
+            this.BtnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // lblGameover
+            // 
+            this.lblGameover.AutoSize = true;
+            this.lblGameover.Font = new System.Drawing.Font("Britannic Bold", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGameover.Location = new System.Drawing.Point(175, 37);
+            this.lblGameover.Name = "lblGameover";
+            this.lblGameover.Size = new System.Drawing.Size(76, 16);
+            this.lblGameover.TabIndex = 4;
+            this.lblGameover.Text = "Game over";
+            this.lblGameover.Visible = false;
+            // 
+            // lblpuntuacion
+            // 
+            this.lblpuntuacion.AutoSize = true;
+            this.lblpuntuacion.Font = new System.Drawing.Font("Britannic Bold", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblpuntuacion.Location = new System.Drawing.Point(355, 37);
+            this.lblpuntuacion.Name = "lblpuntuacion";
+            this.lblpuntuacion.Size = new System.Drawing.Size(20, 16);
+            this.lblpuntuacion.TabIndex = 5;
+            this.lblpuntuacion.Text = "...";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 1;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 299);
+            this.Controls.Add(this.Dino);
+            this.Controls.Add(this.lblpuntuacion);
+            this.Controls.Add(this.lblGameover);
+            this.Controls.Add(this.BtnReset);
+            this.Controls.Add(this.DetectorChoque);
+            this.Controls.Add(this.Carretera);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.Carretera)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dino)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DetectorChoque)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BtnReset)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Carretera;
+        private System.Windows.Forms.PictureBox Dino;
+        private System.Windows.Forms.PictureBox DetectorChoque;
+        private System.Windows.Forms.PictureBox BtnReset;
+        private System.Windows.Forms.Label lblGameover;
+        private System.Windows.Forms.Label lblpuntuacion;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ dinosario chrome mamalon/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dinosario_chrome_mamalon
+{
+    public partial class Form1 : Form
+    {
+        int CambioApariencia;
+        int PosicionInicialPisoY;
+        int PosicionInicialpisoX;
+        int velocidad = 5;
+
+        bool BrincoDelDino;
+        bool InicioJuego = true;
+
+        Random RnTipoObstaculo;
+        List<PictureBox> ListaObstaculos;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void IniciarJuego()
+        {
+            CambioApariencia = 0;
+            BrincoDelDino = false;
+            lblpuntuacion.Text = "0";
+            RnTipoObstaculo = new Random();
+            ListaObstaculos = new List<PictureBox>();
+
+            CrearObstaculo(ListaObstaculos, this);
+            PosicionInicialpisoX = 12;
+            PosicionInicialPisoY = 197;
+
+        }
+        public void CrearObstaculo(List<PictureBox> ListaObstaculos, Form panelJuegoUno)
+        {
+            int TipoObstaculo = RnTipoObstaculo.Next(1, 3);
+            PictureBox pb = new PictureBox();
+            int PosicionObstaculo = (TipoObstaculo == 1) ? 190 : 201; ;
+            pb.Image = Image.FromFile(Application.StartupPath + ((@"\ImgDino\Shrek.jpg")));  
+            pb.BackColor = Color.Transparent;
+            pb.SizeMode = PictureBoxSizeMode.StretchImage;
+            ListaObstaculos.Add(pb);
+            panelJuegoUno.Controls.Add(pb);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!BrincoDelDino)
+            {
+                //Animacion del dino
+                // Dino.Image=(CambioApariencia==0)? Properties
+            }
+            DetectorChoque.Location = Dino.Location;
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            //El dino Brinco
+            if (BrincoDelDino)
+            {
+                int Brinco = Dino.Location.Y - 10;
+                Dino.Location = new Point(Dino.Location.X, Brinco);
+                if (Brinco <= 10)
+                {
+                    BrincoDelDino = false;
+                }
+            }
+            else
+            {
+                if (Dino.Location.Y < PosicionInicialPisoY)
+                {
+                    int Brinco = Dino.Location.Y + 10;
+                    Dino.Location = new Point(Dino.Location.X, Brinco);
+
+                }
+            }
+            //animacion de carretera
+            DetectorChoque.Location = Dino.Location;
+            int Movimiento = Carretera.Location.X - 10;
+            Carretera.Location = new Point(Movimiento, Carretera.Location.Y);
+            //cuando se acabe reinicia
+            if (Carretera.Location.X < -200)
+            {
+                Carretera.Location = new Point(PosicionInicialpisoX, PosicionInicialPisoY);
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            //leer obstaculos generados
+            for (int i = 0; i < ListaObstaculos.Count; i = i + 1)
+            {
+                int MovimientoX;
+                MovimientoX = ListaObstaculos[i].Location.X;
+                MovimientoX = MovimientoX - velocidad;
+                ListaObstaculos[i].Location = new Point(MovimientoX, ListaObstaculos[i].Location.Y);
+
+            }
+            //hay elementos  en el obstaculo
+            if (ListaObstaculos.Count > 0)
+            {
+                if (ListaObstaculos[(ListaObstaculos.Count) - 1].Location.X < -300)
+                {
+                    CrearObstaculo(ListaObstaculos, this);
+                }
+            }
+            if (ListaObstaculos.Count > 0)
+            {
+                for (int i = 0; i < ListaObstaculos.Count; i = i + 1)
+                {
+                    if (ListaObstaculos[i].Location.X < Dino.Location.Y)//si el obstaculo ya paso
+                    {
+                        lblpuntuacion.Text = Convert.ToString(Convert.ToInt32(lblpuntuacion.Text) + 1);
+                    }
+                    this.Controls.Remove(ListaObstaculos[i]);
+                    ListaObstaculos.Remove(ListaObstaculos[i]);
+
+                    //si hay choques con los obstaculos
+                    if (ListaObstaculos[i].Bounds.IntersectsWith(DetectorChoque.Bounds))
+                    {
+                        timer1.Stop(); timer2.Stop(); timer3.Stop(); BtnReset.Visible = true; lblGameover.Visible = true;
+                        this.Controls.Remove(ListaObstaculos[i]);
+                        ListaObstaculos.Remove(ListaObstaculos[i]);
+                    }
+
+                }
+            }
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            IniciarJuego();
+            timer1.Start();
+            timer2.Start();
+            timer3.Start();
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar == Convert.ToChar(Keys.Space)) && (BrincoDelDino == false))
+                //si ya tocaste el espacio
+            {
+                if (InicioJuego)
+                {
+                    IniciarJuego();
+                    InicioJuego = false;
+                    timer1.Enabled = false;
+                    timer2.Enabled = false;
+                    timer3.Enabled = false;
+                }
+                if (Dino.Location.Y>=PosicionInicialPisoY)
+                {
+                    BrincoDelDino = true;
+                }
+            }
+        }
+    }
+}
+
